acd44bf2f1da68baeee328c1dc64189dcbc8f053
Ihor Dobrovolskyi <Ihor.dobrovolskyi@gmail.com>
2025-12-11 18:57:57 +0200

    feat: rename fixture file

--- tests/validation/tui.passenger.page.fields.validation.spec.ts
+++ tests/validation/tui.passenger.page.fields.validation.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../../fixtures';
+import { test, expect } from '../../fixtures/extendedTestfixture';
 
 test.describe('Passenger Details Page Validation Tests', () => {
 
--- tests/validation/tui.second.broker.fields.validation.spec.ts
+++ tests/validation/tui.second.broker.fields.validation.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../../fixtures';
+import { test, expect } from '../../fixtures/extendedTestfixture';
 
 test.describe('Second Passenger validation Tests', () => {
 
